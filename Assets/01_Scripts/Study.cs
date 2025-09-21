abc416a310d93e35e2d087124b99657033dfcd21
yeonwoo0981 <0981syw@naver.com>
2025-09-21 23:25:55 +0900

    Study Commit
    공부용 코드 추가

---
+++ Assets/01_Scripts/Study.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Study : MonoBehaviour
+{
+    [Header("Jeongsu")]
+    [SerializeField] private int jeongsu = 1;
+    [SerializeField] private int jeongsu2 = 2;
+
+    [Header("Sylsu")]
+    [SerializeField] private float sylsu = 1f;
+    [SerializeField] private float sylsu2 = 2f;
+
+    [Header("String")]
+    [SerializeField] private string str = "문자열1";
+    [SerializeField] private string str2 = "문자열2";
+}
